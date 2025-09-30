6fb7d705df70725060fc0db13f3c3b8b26f271de
Taaniel Vananurm <taaniel.vananurm@tptlive.ee>
2025-09-30 15:32:50 +0300

    Posts, public functions

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -7,7 +7,10 @@ use Illuminate\Http\Request;
 class PublicController extends Controller
 {
     public function index() {
-        return view('welcome');
+        $page = request()->query('page', 1); // p1 => 0, p2 => 16, p3 => 32, ...
+        $offset = ($page - 1) * 16;
+        $posts = Post::take(16)->skip($offset)->get();
+        return view('welcome', compact('posts'));
     }
 
     public function page1() {
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,4 +9,10 @@ class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+
+    protected function snippet(): Attribute {
+        return Attribute::get(function () {
+            return explode("/n/n", $this->body)[0];
+        });
+    }
 }
